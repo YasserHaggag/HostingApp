b25cd50d95c9bccf3270924b0c50352ed1de421b
Yasser Hassan <E-y.ali@lean.sa>
2022-06-11 01:03:10 +0300

    changes on scripts to be fit with Windows commands

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -4,8 +4,9 @@ dotenv.config();
 // - AWS_ACCESS_KEY_ID
 // - AWS_SECRET_ACCESS_KEY
 // Are Also needed
-
+console.log(process.env._port)
 export const config = {
+  
   username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
