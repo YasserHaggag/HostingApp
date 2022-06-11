733b3ac002e73f12a931a9adb3fcfc8aa2e98d2d
Yasser Hassan <E-y.ali@lean.sa>
2022-06-12 02:28:49 +0300

    change the deploy.sh file

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,3 @@
-aws s3 cp --region us-east-1 --recursive --acl public-read ./www s3://yasser-udagram/
+aws s3 cp --recursive --acl public-read www s3://yasser-udagram/
+#aws s3 cp --region us-east-1 --recursive --acl public-read ./www s3://yasser-udagram/
 #aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://yasser-udagram/
