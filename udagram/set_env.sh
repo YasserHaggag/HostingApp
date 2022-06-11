aeccce55b5896fda6727d43aeb779bca527134a9
Yasser Hassan <E-y.ali@lean.sa>
2022-06-12 00:59:26 +0300

    change the set_env.sh file

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,15 +1,16 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-2.czpmybbqwycj.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::yasser-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
-
+export URL=Yasserapp-env.eba-fb6x9t3q.us-east-1.elasticbeanstalk.com/
+export AWS_ACCESS_KEY_ID: AKIAWC64XQ6KCNEJ4HMW
+export AWS_SECRET_ACCESS_KEY: xctkBI3XYrnEJUNg4pVMm8n6/6yvJi1/RYT8WycBeG0=
 
 
 database user :postgres
